14f244aa9a32d23bb675cd11c55ef20d0a585f84
gamageg <gayathri.gamage@assurity.co.nz>
2023-05-01 14:53:47 +1200

    message update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("NZ peers Hello world!");
+        System.out.println("NZ peers folks Hello world!");
     }
 }
