69b73d5a598c004aed8de6654d0230b41455c818
gamageg <gayathri.gamage@assurity.co.nz>
2023-05-02 14:58:32 +1200

    reverted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("today hi NZ peers folks and Hello world!")
+        System.out.println("today hi NZ peers folks and Hello world!");
     }
 }
